48079ae7f1bbad9981f9241ddbaf4eb28de64cfd
David Ready <davis_ready@hotmail.com>
2021-03-17 14:53:40 -0400

    dependencies and set up PORT

---
+++ server.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+const express = require("express");
+
+const app = express();
+
+const PORT = process.env.PORT || 3000
+
+
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}!`);
+  });
+
