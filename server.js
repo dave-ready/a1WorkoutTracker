19f71ea8e69ad363d7408fa116c69c012e510cfc
David Ready <davis_ready@hotmail.com>
2021-03-17 15:38:41 -0400

    filename edit

--- server.js
+++ server.js
@@ -6,6 +6,27 @@ const app = express();
 
 const PORT = process.env.PORT || 3000
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(morgan("dev"));
+app.use(express.static('public'));
+
+
+mongoose.connect(
+    process.env.MONGODB_URI || 'mongodb://localhost/workout',
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false
+    }
+  );
+  
+
+require('./routes/api-routes')(app)
+require('./routes/html-routes')(app)
+
+
 
 app.listen(PORT, () => {
     console.log(`App running on port ${PORT}!`);
