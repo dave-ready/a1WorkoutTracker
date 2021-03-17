94c4dba2422edc013bc912a8d4e04f07ccf81dd3
David Ready <davis_ready@hotmail.com>
2021-03-17 17:10:37 -0400

    workoutSchema

--- models/workout.js
+++ models/workout.js
@@ -1,10 +1,38 @@
 const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
-const workoutSchema = new schema({
-   name: { type: String, required: true },
-   workout: { type: Array }
-});
+
+const workoutSchema = new Schema({
+    day: { 
+        type: Date, 
+        required: true 
+    },
+    exercises: [
+      {
+        name: {
+            type: String,
+            required: true,
+            trim: true
+          },
+        type: {
+          type: String,
+          trim: true
+        },
+        duration: {
+          type: Number
+        },
+        weight: {
+          type: Number
+        },
+        reps: {
+          type: Number
+        },
+        sets: {
+          type: Number
+        }
+      }
+    ]
+  });
 
   
   const Workout = mongoose.model("Workout", workoutSchema);
